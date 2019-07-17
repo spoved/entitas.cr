c3f3fd0303ff45b388844cc335cc0c811414df34
Holden Omans <holden.omans@gmail.com>
2019-07-17 18:26:53 -0400

    Added context info tests

--- build.sh
+++ build.sh
@@ -7,5 +7,4 @@ crystal tool format
 
 ./bin/ameba
 ./bin/crystal-coverage spec/entitas/*.cr spec/entitas/**/*.cr
-
-# crystal spec --error-trace
+crystal spec --error-trace
---
+++ spec/entitas/context/info_spec.cr
@@ -0,0 +1,15 @@
+require "../../spec_helper"
+
+describe Entitas::Context::Info do
+  describe "when created" do
+    it "sets fields with constructor values" do
+      name = "My Context"
+      comp_names = ["Health", "Position", "View"]
+      comp_types = [A, B, C]
+      info = Entitas::Context::Info.new(name, comp_names, comp_types)
+      info.name.should eq name
+      info.component_names.should eq comp_names
+      info.component_types.should eq comp_types
+    end
+  end
+end
