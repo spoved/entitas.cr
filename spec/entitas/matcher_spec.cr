688462e60eb5e4827dcb01cd1d27c891d36971d6
Holden Omans <holden.omans@gmail.com>
2019-07-23 18:14:29 -0400

    more specs

--- spec/entitas/matcher_spec.cr
+++ spec/entitas/matcher_spec.cr
@@ -43,6 +43,14 @@ private def all_of_none_of
   Entitas::Matcher(TestEntity).all_of(A, B).none_of(C, D)
 end
 
+private def any_of_none_of
+  Entitas::Matcher(TestEntity).any_of(A, B).none_of(C, D)
+end
+
+private def all_of_any_of
+  Entitas::Matcher(TestEntity).all_of(A, B).any_of(C, D)
+end
+
 describe Entitas::Matcher do
   eA, eB, eC, eAB, eABC = new_entities
 
@@ -268,6 +276,155 @@ describe Entitas::Matcher do
     end
   end
 
-  describe "any_of.none_of" do
+  describe "#any_of.none_of" do
+    it "has all indices" do
+      m = any_of_none_of
+
+      assert_indices_contain m.indices, [
+        ::Entitas::Component::Index::A,
+        ::Entitas::Component::Index::B,
+        ::Entitas::Component::Index::C,
+        ::Entitas::Component::Index::D,
+      ]
+      assert_indices_contain m.any_of_indices, [::Entitas::Component::Index::A, ::Entitas::Component::Index::B]
+      assert_indices_contain m.none_of_indices, [::Entitas::Component::Index::C, ::Entitas::Component::Index::D]
+    end
+
+    it "has all indices without duplicates" do
+      m = Entitas::Matcher(TestEntity).any_of(A, A, B).none_of(B, C, C)
+
+      assert_indices_contain m.indices, [
+        ::Entitas::Component::Index::A,
+        ::Entitas::Component::Index::B,
+        ::Entitas::Component::Index::C,
+      ]
+      assert_indices_contain m.any_of_indices, [::Entitas::Component::Index::A, ::Entitas::Component::Index::B]
+      assert_indices_contain m.none_of_indices, [::Entitas::Component::Index::B, ::Entitas::Component::Index::C]
+    end
+
+    it "caches indices" do
+      m = any_of_none_of
+      m.indices.should be m.indices
+    end
+
+    it "doesn't match" do
+      any_of_none_of.matches?(eABC).should be_false
+    end
+
+    it "matches" do
+      m = any_of_none_of
+      m.matches?(eA).should be_true
+      m.matches?(eB).should be_true
+    end
+
+    it "mutates existing matcher" do
+      m1 = Entitas::Matcher(TestEntity).any_of(A)
+      m2 = m1.none_of(B)
+      m1.should be m2
+
+      assert_indices_contain m1.indices, [
+        ::Entitas::Component::Index::A,
+        ::Entitas::Component::Index::B,
+      ]
+      assert_indices_contain m1.any_of_indices, [::Entitas::Component::Index::A]
+      assert_indices_contain m1.none_of_indices, [::Entitas::Component::Index::B]
+    end
+
+    it "mutates existing merged matcher" do
+      m1 = Entitas::Matcher(TestEntity).all_of(A)
+      m2 = Entitas::Matcher(TestEntity).all_of(B)
+      m3 = Entitas::Matcher(TestEntity).any_of(m1)
+      m4 = m3.none_of(m2)
+
+      m3.should be m4
+
+      assert_indices_contain m3.indices, [::Entitas::Component::Index::A, ::Entitas::Component::Index::B]
+      assert_indices_contain m3.any_of_indices, [::Entitas::Component::Index::A]
+      assert_indices_contain m3.none_of_indices, [::Entitas::Component::Index::B]
+    end
+
+    it "can to_s" do
+      any_of_none_of.to_s.should eq "AnyOf(0, 1).NoneOf(2, 3)"
+    end
+
+    it "uses component_names when component_names set" do
+      matcher = any_of_none_of
+      matcher.component_names = ["one", "two", "three", "four", "five"]
+      matcher.to_s.should eq "AnyOf(one, two).NoneOf(three, four)"
+    end
+  end
+
+  describe "#all_of.any_of" do
+    it "has all indices" do
+      m = all_of_any_of
+      assert_indices_contain m.indices, [
+        ::Entitas::Component::Index::A,
+        ::Entitas::Component::Index::B,
+        ::Entitas::Component::Index::C,
+        ::Entitas::Component::Index::D,
+      ]
+      assert_indices_contain m.all_of_indices, [::Entitas::Component::Index::A, ::Entitas::Component::Index::B]
+      assert_indices_contain m.any_of_indices, [::Entitas::Component::Index::C, ::Entitas::Component::Index::D]
+    end
+
+    it "has all indices without duplicates" do
+      m = Entitas::Matcher(TestEntity).all_of(A, A, B).any_of(B, C, C)
+      assert_indices_contain m.indices, [
+        ::Entitas::Component::Index::A,
+        ::Entitas::Component::Index::B,
+        ::Entitas::Component::Index::C,
+      ]
+      assert_indices_contain m.all_of_indices, [::Entitas::Component::Index::A, ::Entitas::Component::Index::B]
+      assert_indices_contain m.any_of_indices, [::Entitas::Component::Index::B, ::Entitas::Component::Index::C]
+    end
+
+    it "caches indices" do
+      m = all_of_any_of
+      m.indices.should be m.indices
+    end
+
+    it "doesn't match" do
+      all_of_any_of.matches?(eAB).should be_false
+    end
+
+    it "matches" do
+      m = all_of_any_of
+      m.matches?(eABC).should be_true
+    end
+
+    it "mutates existing matcher" do
+      m1 = Entitas::Matcher(TestEntity).all_of(A)
+      m2 = m1.any_of(B)
+      m1.should be m2
+      assert_indices_contain m1.indices, [
+        ::Entitas::Component::Index::A,
+        ::Entitas::Component::Index::B,
+      ]
+      assert_indices_contain m1.all_of_indices, [::Entitas::Component::Index::A]
+      assert_indices_contain m1.any_of_indices, [::Entitas::Component::Index::B]
+    end
+
+    it "mutates existing merged matcher" do
+      m1 = Entitas::Matcher(TestEntity).all_of(A)
+      m2 = Entitas::Matcher(TestEntity).all_of(B)
+      m3 = Entitas::Matcher(TestEntity).all_of(m1)
+      m4 = m3.any_of(m2)
+
+      m3.should be m4
+
+      assert_indices_contain m3.indices, [::Entitas::Component::Index::A, ::Entitas::Component::Index::B]
+      assert_indices_contain m3.all_of_indices, [::Entitas::Component::Index::A]
+      assert_indices_contain m3.any_of_indices, [::Entitas::Component::Index::B]
+    end
+
+    it "can to_s" do
+      all_of_any_of.to_s.should eq "AllOf(0, 1).AnyOf(2, 3)"
+    end
+
+    it "uses component_names when component_names set" do
+      matcher = all_of_any_of
+      matcher.component_names = ["one", "two", "three", "four", "five"]
+      matcher.to_s.should eq "AllOf(one, two).AnyOf(three, four)"
+    end
   end
 end
