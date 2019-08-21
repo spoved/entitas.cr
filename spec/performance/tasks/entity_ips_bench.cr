9c765e01be39f2f762735e3bea1c70554e373274
Holden Omans <holden.omans@gmail.com>
2019-08-21 11:21:30 -0400

    performance improvements

--- spec/performance/tasks/entity_ips_bench.cr
+++ spec/performance/tasks/entity_ips_bench.cr
@@ -45,4 +45,50 @@ start_bench ::Entitas::Entity, ->do
   }, ->{
     e.replace_component(Entitas::Component::Index::A, e.create_component(Entitas::Component::Index::A))
   }, ->{}, true
+
+  group "Event triggers", ->do
+    bench "#on_component_replaced_event_hooks.each", ->{
+      ctx = TestContext.new
+      entity = ctx.create_entity.add_a
+      entity.on_component_replaced do
+        nil
+      end
+      event = Entitas::Events::OnComponentReplaced.new(entity, 1, entity.a, A.new)
+    }, ->{
+      entity.on_component_replaced_event_hooks.each &.call(event)
+    }, ->{}, true
+
+    bench "#on_component_replaced_event_hooks.reverse.each", ->{
+      ctx = TestContext.new
+      entity = ctx.create_entity.add_a
+      entity.on_component_replaced do
+        nil
+      end
+      event = Entitas::Events::OnComponentReplaced.new(entity, 1, entity.a, A.new)
+    }, ->{
+      entity.on_component_replaced_event_hooks.reverse.each &.call(event)
+    }, ->{}, true
+
+    bench "#receive_on_component_replaced_event", ->{
+      ctx = TestContext.new
+      entity = ctx.create_entity.add_a
+      entity.on_component_replaced do
+        nil
+      end
+      event = Entitas::Events::OnComponentReplaced.new(entity, 1, entity.a, A.new)
+    }, ->{
+      entity.receive_on_component_replaced_event(event)
+    }, ->{}, true
+
+    bench "#receive_on_component_added_event", ->{
+      ctx = TestContext.new
+      entity = ctx.create_entity.add_a
+      entity.on_component_added do
+        nil
+      end
+      event = Entitas::Events::OnComponentAdded.new(entity, 1, A.new)
+    }, ->{
+      entity.receive_on_component_added_event(event)
+    }, ->{}, true
+  end
 end
