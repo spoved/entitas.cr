49b1096b704c8f29a1cddf0c8d4495984278b5c0
Holden Omans <holden.omans@gmail.com>
2019-09-13 19:58:29 -0400

    Added stat server and example index.html to view

--- src/entitas/controller.cr
+++ src/entitas/controller.cr
@@ -1,3 +1,6 @@
+require "http/server"
+require "json"
+
 module Entitas
   abstract class Controller
     private property systems : Systems? = nil
@@ -18,5 +21,24 @@ module Entitas
     end
 
     abstract def create_systems(contexts : Contexts)
+
+    def stats
+      contexts.as(Contexts).all_contexts
+    end
+
+    def start_server
+      spawn do
+        server = HTTP::Server.new do |context|
+          context.response.content_type = "application/json"
+          context.response.headers["Access-Control-Allow-Origin"] = "*"
+          context.response.headers["Access-Control-Allow-Methods"] = "GET"
+          context.response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+          context.response.print (stats).to_json
+        end
+
+        puts "Listening on http://127.0.0.1:8080"
+        server.listen(8080)
+      end
+    end
   end
 end
